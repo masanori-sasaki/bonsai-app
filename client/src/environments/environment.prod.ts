0ec01382c8af81629f30eb986360500dd8375c4d
masanori-sasaki <masanori399@gmail.com>
2025-03-19 22:58:51 +0900

    環境回り整備

--- client/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://dmu5lvsxno2tujowgbm7xerasy0eyvqe.lambda-url.ap-northeast-1.on.aws/',
-  cognito: {
-    userPoolId: 'ap-northeast-1_UNhPh5btt',
-    userPoolWebClientId: 'mr5tfnqnajdvdluhq80m100ja',
-    domain: 'bonsai-prod-171278323216.auth.ap-northeast-1.amazoncognito.com'
-  }
-};
